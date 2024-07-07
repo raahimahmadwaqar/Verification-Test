b9ca9976e82292ea64759a5aa93c5ae2a961af60
raahimahmadwaqar <raahimahmed07@gmail.com>
2024-07-07 19:22:33 +0500

    2nd

--- install-argo-workflows.sh
+++ install-argo-workflows.sh
@@ -9,9 +9,9 @@ fi
 ARGO_WORKFLOWS_VERSION=$1
 
 # Create namespace for Argo Workflows
-kubectl create namespace argo
+kubectl create namespace argo-workflows
 
 # Apply the quick-start manifest
-kubectl apply -n argo -f "https://github.com/argoproj/argo-workflows/releases/download/${ARGO_WORKFLOWS_VERSION}/quick-start-minimal.yaml"
+kubectl apply -n argo-workflows -f "https://github.com/argoproj/argo-workflows/releases/download/${ARGO_WORKFLOWS_VERSION}/quick-start-minimal.yaml"
 
-echo "Argo Workflows version ${ARGO_WORKFLOWS_VERSION} installed successfully"
+echo "Argo Workflows version ${ARGO_WORKFLOWS_VERSION} installed successfully in namespace argo-workflows"
